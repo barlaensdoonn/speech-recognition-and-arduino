0a99e66bc74946de99c47b6f9beaffffb27f0675
barlaensdoonn <plump@plump.local>
2016-10-19 13:00:38 -0700

    working python and arduino interface

--- arduino/main_door/main_door.ino
+++ arduino/main_door/main_door.ino
@@ -1,14 +1,12 @@
-// ---------------------------------------------------------------------------
-// This example shows how to use NewPing's ping_timer method which uses the Timer2 interrupt to get the
-// ping time. The advantage of using this method over the standard ping method is that it permits a more
-// event-driven sketch which allows you to appear to do two things at once. An example would be to ping
-// an ultrasonic sensor for a possible collision while at the same time navigating. This allows a
-// properly developed sketch to multitask. Be aware that because the ping_timer method uses Timer2,
-// other features or libraries that also use Timer2 would be effected. For example, the PWM function on
-// pins 3 & 11 on Arduino Uno (pins 9 and 11 on Arduino Mega) and the Tone library. Note, only the PWM
-// functionality of the pins is lost (as they use Timer2 to do PWM), the pins are still available to use.
-// NOTE: For Teensy/Leonardo (ATmega32U4) the library uses Timer4 instead of Timer2.
-// ---------------------------------------------------------------------------
+// THE DOOR
+// 10/19/16
+
+// open the grate when ultrasonic sensor is triggered
+// leave open 30 seconds
+// close grate and pause 3 seconds
+// open door for 15 seconds
+// close door, wait 15 seconds before grate can be triggered again
+
 #include <NewPing.h>
 
 #define TRIGGER_PIN   12 // Arduino pin tied to trigger pin on ping sensor.
@@ -17,83 +15,87 @@
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
 
-unsigned int pingSpeed = 50; // How frequently are we going to send out a ping (in milliseconds). 50ms would be 20 times a second.
-unsigned long pingTimer;     // Holds the next ping time.
-unsigned long doorTimer = 0;
-unsigned long doorOpenTime = 0;
-unsigned long doorCloseTime = 0;
-//unsigned long doorInterval = 15000;
+unsigned long pingTimer;
+// unsigned long grateTimer;
+unsigned long gratePauseTimer; 
+unsigned long doorTimer;
+unsigned long doorPauseTimer;
 
-int distance = 0;
+// all times here are in milliseconds //
+unsigned int pingSpeed = 50;      // ping frequency. 50ms would be 20 times a second.
+const long doorPause = 3000;      // amount of time after grate closing that door pauses before opening
+const long doorClose = 18000;     // doorPause - doorClose = amount of time door stays open
+const long gratePause = 15000;    // amount of time after door closing before grate can be triggered again
+
 int incoming;
+int distance = 0;
+int gratePin = 7;
+int doorPin = 8;
 
-int gratePin = 13;
-int doorPin = 5;
+bool grateState = false;
+bool doorState = false;
+bool doorOpen = false;
 
-boolean grateState = false;
-boolean doorState = false;
-boolean doorOpen = false;
 
 void setup() {
-  Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
+  Serial.begin(115200);
   while (!Serial){
     ;
   }
-
-  pingTimer = millis(); // Start now.
+  
   pinMode(gratePin, OUTPUT);
   pinMode(doorPin, OUTPUT);
   digitalWrite(gratePin, LOW);
   digitalWrite(doorPin, LOW);
+
+  pingTimer = millis();
+  gratePauseTimer = millis();
+  doorTimer = millis();
+  doorPauseTimer = millis();
 }
 
 void loop() {
-  // check ping distance every 50ms
   if (millis() >= pingTimer) {   // pingSpeed milliseconds since last ping, do another ping.
     pingTimer += pingSpeed;      // Set the next ping time.
     sonar.ping_timer(echoCheck); // Send out the ping, calls "echoCheck" function every 24uS where you can check the ping status.
   }
 
-//  Serial.print("Ping: ");
-//  Serial.print(distance);
-//  Serial.println("cm");
-
-  if (distance <= 25 && (!grateState) && (!doorState)){
-    grateState = true;
-    //doorTimer = millis() + 1000;
+  if (distance <= 50 && !grateState && !doorState && millis() >= gratePauseTimer) {
     Serial.write('1');
     digitalWrite(gratePin, HIGH);
+    grateState = true;
+    // Serial.println("grate triggered");
   }
-  else if (grateState){
+  else if (grateState) {
     if (Serial.available() > 0){
       incoming = Serial.read();
       if (incoming == '0'){
         digitalWrite(gratePin, LOW);
         grateState = false;
-        //doorState = true;
+        doorState = true;
       }
     }
+    // Serial.println("grate off/door on");
   }
-//  else if (doorState){
-//    digitalWrite(doorPin, HIGH);
-//    digitalWrite(doorPin, LOW);
-//  }
-//    if ((millis() >= doorTimer) && (!doorOpen)){
-//      doorTimer = millis() + 16000;
-//      doorOpenTime = millis() + 1000;
-//      doorCloseTime = millis() + 11000;
-//    }
-//    else if ((millis() >= doorOpenTime) && (!doorOpen)){
-//      digitalWrite(doorPin, HIGH);
-//      doorOpen = true;
-//    }
-//    else if ((millis() >= doorCloseTime) && (doorOpen)){
-//      digitalWrite(doorPin, LOW);
-//    }
-//    else if ((millis() >= doorTimer) && (doorOpen)){
-//      doorOpen = false;
-//      doorState = false;
-//    }
+  else if (doorState) {
+    if (millis() >= doorTimer && !doorOpen) {
+      doorPauseTimer = millis() + doorPause;
+      doorTimer = millis() + doorClose;
+      // Serial.println("door timers set");
+    }
+    else if (millis() >= doorPauseTimer && !doorOpen) {
+      digitalWrite(doorPin, HIGH);
+      doorOpen = true;
+      // Serial.println("door open");
+    }
+    else if (millis() >= doorTimer && doorOpen) {
+      digitalWrite(doorPin, LOW);
+      doorOpen = false;
+      doorState = false;
+      gratePauseTimer = millis() + gratePause;
+      // Serial.println("door closed");
+    }
+  }
   
 }
 
